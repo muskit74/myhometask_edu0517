449136b99d0171d5df15a5be251922e35a8de8e3
muskit74 <66120776+muskit74@users.noreply.github.com>
2020-06-09 13:46:38 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -14,11 +14,7 @@
 public class Main {
     public static void main(String[] args) {
         String [] arr = {"Мама", "Мыла", "Раму"};
-        System.out.println(arr[0]+arr[1]+arr[2]);
-        System.out.println(arr[0]+arr[2]+arr[1]);
-        System.out.println(arr[1]+arr[0]+arr[2]);
-        System.out.println(arr[1]+arr[2]+arr[0]);
-        System.out.println(arr[2]+arr[0]+arr[1]);
-        System.out.println(arr[2]+arr[1]+arr[0]);
-    }
+        for(int i=0; i<3; i++)
+            for(int j=0; j<3; j++)
+                if (i!=j) System.out.println(arr[i]+arr[j]+arr[3-i-j]);}
 }
