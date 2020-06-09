a5df4dd1e5076fea5fd44b75b0fb5d324c1fca6a
muskit74 <66120776+muskit74@users.noreply.github.com>
2020-06-09 13:40:38 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -12,7 +12,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1*2*3*4*5*6*7*8*9*10);
-    }
-// можно заморочиться с циклом, конечно, но в условии об этом не сказано
+        int prod = 1;
+        for(int i=1; i<11; i++){
+            prod = prod*i;            }
+        System.out.println(prod);}
 }
